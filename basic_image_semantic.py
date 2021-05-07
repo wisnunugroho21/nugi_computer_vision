c2a8aad0e19232e70da2c31e54730f8f5269c38e
Nugroho Dewantoro <nugroho8dewantoro@gmail.com>
2021-05-07 20:20:27 +0800

    update

--- basic_image_semantic.py
+++ basic_image_semantic.py
@@ -2,11 +2,25 @@ import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 
-from dataloader.CatsDataset import CatsDataset
+from dataloader.PennFudanPedDataset import PennFudanPedDataset
+from dataloader.ClrPennFudanPedDataset import ClrPennFudanPedDataset
+
 from loss.simclr import SimCLR
 from model.image_semantic.encoder import Encoder
 from model.image_semantic.decoder import Decoder
 from model.clr.projection import Projection
+
+import matplotlib.pyplot as plt
+
+def display(display_list, title):
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i + 1)
+        plt.title(title[i])
+
+        disImg  = display_list[i].detach().numpy()
+        plt.imshow(disImg)
+        plt.axis('off')
+    plt.show()
 
 PATH = '.'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -28,11 +42,7 @@ trans1 = transforms.Compose([
 ])
 
 trans2 = transforms.Compose([
-    transforms.Resize((256, 256)),    
-    transforms.RandomResizedCrop(256),                           
-    transforms.RandomApply([transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)], p = 0.8),
-    transforms.RandomGrayscale(p = 0.2),
-    transforms.GaussianBlur(25),
+    transforms.Resize((256, 256)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
@@ -41,45 +51,47 @@ trans_label  = transforms.Compose([
     transforms.Resize((256, 256))
 ])
 
-trainset    = CatsDataset(root = 'dataset/cats', transforms1 = trans0, transforms2 = trans_label)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size = 8, shuffle = False, num_workers = 8)
+clrset      = ClrPennFudanPedDataset(root = 'drive/My Drive/datasets/dataset', transforms1 = trans1, transforms2 = trans2)
+clrloader   = torch.utils.data.DataLoader(clrset, batch_size = 16, shuffle = True, num_workers = 2)
 
-clrset1     = CatsDataset(root = 'dataset/cats', transforms1 = trans1, transforms2 = trans_label)
-clrloader1  = torch.utils.data.DataLoader(clrset1, batch_size = 8, shuffle = False, num_workers = 8)
+trainset    = PennFudanPedDataset(root = 'drive/My Drive/datasets/dataset', transforms1 = trans0, transforms2 = trans_label)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size = 16, shuffle = True, num_workers = 2)
 
-clrset2     = CatsDataset(root = 'dataset/cats', transforms1 = trans2, transforms2 = trans_label)
-clrloader2  = torch.utils.data.DataLoader(clrset2, batch_size = 8, shuffle = False, num_workers = 8)
-
-testset     = CatsDataset(root = 'dataset/cats', transforms1 = trans0, transforms2 = trans_label)
-testloader  = torch.utils.data.DataLoader(testset, batch_size = 8, shuffle = False, num_workers = 8)
+testset     = PennFudanPedDataset(root = 'drive/My Drive/datasets/dataset', transforms1 = trans0, transforms2 = trans_label)
+testloader  = torch.utils.data.DataLoader(testset, batch_size = 16, shuffle = False, num_workers = 2)
 
 encoder     = Encoder()
 projector   = Projection()
 
-encoder, projector = encoder.to(device), projector.to(device)
+encoder1     = Encoder()
+projector1   = Projection()
+
+encoder, projector    = encoder.to(device), projector.to(device)
+encoder1, projector1  = encoder1.to(device), projector1.to(device)
+
+encoder1.load_state_dict(encoder1.state_dict())
+projector1.load_state_dict(projector1.state_dict())
 
 clroptimizer    = torch.optim.Adam(list(encoder.parameters()) + list(projector.parameters()), lr = 0.001)
 clrscaler       = torch.cuda.amp.GradScaler()
-clrloss         = SimCLR(True)
+clrloss         = SimCLR()
 
-for epoch in range(5):
+for epoch in range(1):
     running_loss = 0.0
-    for data1, data2 in zip(clrloader1, clrloader2):
-        input1, _   = data1        
-        input2, _   = data2
-
+    for data in clrloader:
+        input1, input2  = data
         input1, input2  = input1.to(device), input2.to(device)
 
         clroptimizer.zero_grad()
-
         with torch.cuda.amp.autocast():
             mid1   = encoder(input1).mean([2, 3])
             out1   = projector(mid1)
 
-            mid2   = encoder(input2).mean([2, 3])
-            out2   = projector(mid2)
+            mid2   = encoder1(input2).mean([2, 3])
+            out2   = projector1(mid2)
 
-            loss = (clrloss.compute_loss(out1, out2) + clrloss.compute_loss(out2, out1)) / 2.0
+            loss = clrloss.compute_loss(out1, out2.detach())
+
         clrscaler.scale(loss).backward()
         clrscaler.step(clroptimizer)
         clrscaler.update()
@@ -89,35 +101,32 @@ for epoch in range(5):
 print('Finished Pre-Training')
 torch.save(encoder.state_dict(), PATH + '/encoder.pth')
 
-decoder = Decoder()
+# -----------------------------------------------------------------------------------------------------------
+
+decoder = Decoder(2)
 decoder = decoder.to(device)
 
 segoptimizer    = torch.optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr = 0.001)
 segscaler       = torch.cuda.amp.GradScaler()
 segloss         = nn.CrossEntropyLoss()
 
-for epoch in range(5):
-    running_loss = 0.0
+for epoch in range(1):
     for i, data in enumerate(trainloader, 0):
-        input, label    = data
-        input, label    = input.to(device), label.to(device)
+        images, labels    = data
+        images, labels    = images.to(device), labels.to(device)
 
         segoptimizer.zero_grad()
-
         with torch.cuda.amp.autocast():
-            mid = encoder(input)
+            mid = encoder(images)
             out = decoder(mid)
 
-            loss = segloss(out, label)
+            loss = segloss(out, labels)
 
         segscaler.scale(loss).backward()
         segscaler.step(segoptimizer)
         segscaler.update()
 
-        running_loss += loss.item()
-        if i % 100 == 99:
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 10))
-            running_loss = 0.0
+        print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, loss.item()))
 
 print('Finished Training')
 
@@ -131,14 +140,27 @@ with torch.no_grad():
         images, labels = data
         images, labels = images.to(device), labels.to(device)
 
-        mid = encoder(input)
+        mid = encoder(images)
         out = decoder(mid)
-        predicted = torch.argmax(out, -1)
 
-        total   += labels.size(0)        
-        correct += (predicted == labels).sum().item()
+        total   += (labels.shape[0] * labels.shape[1] * labels.shape[2])
+        correct += (out.argmax(1) == labels).sum().item()
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
 
 torch.save(encoder.state_dict(), PATH + '/encoder.pth')
 torch.save(decoder.state_dict(), PATH + '/decoder.pth')
+
+# -------------------------------------------------------------------
+
+images, labels  = testset[0]
+images          = images.unsqueeze(0)
+images, labels  = images.to(device), labels.to(device)
+
+mid = encoder(images)
+out = decoder(mid)
+
+disInput    = images.squeeze(0).transpose(0, 1).transpose(1, 2)
+disOutput   = out.squeeze(0).transpose(0, 1).transpose(1, 2).argmax(-1)
+
+display([disInput.cpu(), labels.cpu(), disOutput.cpu()], ['Input Image', 'True Mask', 'Predicted Mask'])
--- model/image_semantic/downsampler_encoder.py
+++ model/image_semantic/downsampler_encoder.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from model.component.depthwise_separable_conv2d import DepthwiseSeparableConv2d
 
@@ -5,22 +6,24 @@ class DownsamplerEncoder(nn.Module):
     def __init__(self, dim_in, dim_out):
         super(DownsamplerEncoder, self).__init__()
 
-        self.downsampler1 = nn.Sequential(            
-            DepthwiseSeparableConv2d(dim_in, dim_in, kernel_size = 4, stride = 2, padding = 1, bias = False),
-            nn.ReLU(),
-            DepthwiseSeparableConv2d(dim_in, dim_out, kernel_size = 4, stride = 2, padding = 1, bias = False),
-            nn.ReLU()
+        self.downsampler1 = nn.Sequential(
+            nn.Conv2d(dim_in, dim_in, kernel_size = 4, stride = 2, padding = 1, groups = dim_in, bias = False),
+            nn.Conv2d(dim_in, dim_in, kernel_size = 4, stride = 2, padding = 1, groups = dim_in, bias = False)
         )
         self.downsampler2 = nn.Sequential(
-            DepthwiseSeparableConv2d(dim_in, dim_out, kernel_size = 8, stride = 4, padding = 2, bias = False),
-            nn.ReLU()
+            nn.Conv2d(dim_in, dim_in, kernel_size = 8, stride = 4, padding = 2, groups = dim_in, bias = False),
         )
 
-        self.bn = nn.BatchNorm2d(dim_out)
+        self.out = nn.Sequential(
+            nn.Conv2d(2 * dim_in, dim_out, kernel_size = 1),
+            nn.ReLU()
+        )
 
     def forward(self, x):        
         x1      = self.downsampler1(x)
         x2      = self.downsampler2(x)
 
-        xout    = self.bn(x1 + x2)
+        xout    = torch.cat((x1, x2), 1)
+        xout    = self.out(xout)
+
         return xout
